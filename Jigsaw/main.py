a6bfdd91ef0b3e1eb85638780c3ff8e3c085bc08
Cvicnaire <143851912+Cvicnaire@users.noreply.github.com>
2024-08-02 13:53:05 -0400

    Update main.py

--- Jigsaw/main.py
+++ Jigsaw/main.py
@@ -1,1 +1,8 @@
+from config import workflow_template_file, variables_file, workflow_output_dir, json_output_dir
+from utils.file_operations import read_variables
+from utils.parser import assemble_workflow
+
+if __name__ == "__main__":
+    variables = read_variables(variables_file)
+    assemble_workflow(variables, workflow_template_file, workflow_output_dir, json_output_dir)
 
