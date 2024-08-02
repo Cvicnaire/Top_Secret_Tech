96b433b6ac0b6f366d5a2154363ea6f6bfa8791b
Cvicnaire <143851912+Cvicnaire@users.noreply.github.com>
2024-08-02 13:54:50 -0400

    Update file_operations.py

--- Jigsaw/utils/file_operations.py
+++ Jigsaw/utils/file_operations.py
@@ -1,1 +1,12 @@
+def read_task(task_filename):
+    with open(task_filename, 'r') as file:
+        return file.read()
+
+def read_variables(variable_file):
+    variables = {}
+    with open(variable_file, 'r') as file:
+        for line in file:
+            key, value = line.strip().split('=', 1)
+            variables[key] = value
+    return variables
 
