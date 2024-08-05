e4ef80f763a4f2837c4b2a6bf779ac9e96a874d6
Cvicnaire <143851912+Cvicnaire@users.noreply.github.com>
2024-08-05 15:16:54 -0400

    Package Update

--- Jigsaw/main.py
+++ Jigsaw/main.py
@@ -1,8 +1,12 @@
 """main module to run the workflow assembly"""
-
+import sys
+import os
 from utils.parser import assemble_workflow
 from utils.file_operations import read_variables
 from config import workflow_template_file, variables_file, workflow_output_dir, json_output_dir
+# Add the path to the 'utils' module to the Python module search path
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/utils')
+
 if __name__ == "__main__":
     variables = read_variables(variables_file)
     assemble_workflow(variables, workflow_template_file, workflow_output_dir, json_output_dir)
--- Jigsaw/utils/parser.py
+++ Jigsaw/utils/parser.py
@@ -28,12 +28,13 @@ def parse_task_inputs(inputs):
     for input_block in inputs:
         for line in input_block.splitlines():
             if line.strip():
-                input_type, input_name = re.findall(r"(\w+)\s+(\w+)", line.strip())[0]
+                input_name = re.findall(r"(\w+)\s+(\w+)", line.strip())[0]
                 task_inputs[input_name] = None  # Default value for optional inputs
     return task_inputs
 
-"""Function to assemble the workflow."""
+
 def assemble_workflow(variables, workflow_template_file, workflow_output_dir, json_output_dir):
+    """Function to assemble the workflow."""
     task_files = variables['task_files'].split(',')
     fastqc_flags = variables.get('fastqc_flags', "")
     outdirPath = variables.get('outdirPath', ".")
@@ -46,24 +47,28 @@ def assemble_workflow(variables, workflow_template_file, workflow_output_dir, js
 
     for task_file in task_files:
         task_content = read_task(task_file)
-   
+
         if "task FastQC" in task_content:
             task_content = task_content.replace("${fastqc_flags}", fastqc_flags)
         # where i can add other flags from other packages if needed.
         task_definitions += task_content + "\n\n"
-   
+
         inputs, outputs = extract_task_io(task_content)
         task_name = extract_task_name(task_content)
-   
+
         task_inputs = parse_task_inputs(inputs)
-  
+
         workflow_inputs += generate_workflow_inputs(task_inputs)
         workflow_outputs += generate_workflow_outputs(outputs, task_name)
         task_calls += generate_task_calls(task_inputs, variables, task_name)
         all_inputs.update({f"{task_name}.{key}": value for key, value in task_inputs.items()})
 
     workflow_template = read_workflow_template(workflow_template_file)
-    composed_workflow = replace_placeholders(workflow_template, task_definitions, workflow_inputs, workflow_outputs, task_calls)
+    composed_workflow = replace_placeholders(workflow_template,
+                                             task_definitions,
+                                             workflow_inputs,
+                                             workflow_outputs,
+                                             task_calls)
 
     unique_id = get_next_unique_id()
     output_workflow_file = f"composed_workflow_{unique_id}.wdl"
@@ -76,17 +81,20 @@ def assemble_workflow(variables, workflow_template_file, workflow_output_dir, js
     move_files(json_output_file, json_output_dir)
 
     print(f"Workflow assembled and written to {os.path.join(workflow_output_dir, output_workflow_file)}")
+
     print(f"Inputs JSON written to {os.path.join(json_output_dir, json_output_file)}")
 
     submit_workflow()
 
 def generate_workflow_inputs(task_inputs):
+    """Function to generate workflow inputs."""
     workflow_inputs = ""
     for input_name in task_inputs.keys():
         workflow_inputs += f"    {input_name}? {input_name}\n"
     return workflow_inputs
 
 def generate_workflow_outputs(outputs, task_name):
+    """Function to generate workflow outputs."""
     workflow_outputs = ""
     for output_block in outputs:
         for line in output_block.splitlines():
@@ -96,6 +104,7 @@ def generate_workflow_outputs(outputs, task_name):
     return workflow_outputs
 
 def generate_task_calls(task_inputs, variables, task_name):
+    """Function to generate task calls."""
     task_calls = f"    call {task_name} {{\n"
     for input_name in task_inputs.keys():
         if input_name in variables:
@@ -106,11 +115,13 @@ def generate_task_calls(task_inputs, variables, task_name):
     return task_calls
 
 def read_workflow_template(workflow_template_file):
+    """Function to read the workflow template."""
     with open(workflow_template_file, 'r', encoding="utf-8") as template_file:
         workflow_template = template_file.read()
     return workflow_template
 
 def replace_placeholders(workflow_template, task_definitions, workflow_inputs, workflow_outputs, task_calls):
+    """Function to replace placeholders in the workflow template."""
     composed_workflow = workflow_template.replace("${task_definitions}", task_definitions.strip())
     composed_workflow = composed_workflow.replace("${workflow_inputs}", workflow_inputs.strip())
     composed_workflow = composed_workflow.replace("${workflow_outputs}", workflow_outputs.strip())
@@ -118,12 +129,15 @@ def replace_placeholders(workflow_template, task_definitions, workflow_inputs, w
     return composed_workflow
 
 def write_composed_workflow(composed_workflow, output_workflow_file):
+    """Function to write the composed workflow to a file."""
     with open(output_workflow_file, 'w', encoding="utf-8") as output_file:
         output_file.write(composed_workflow)
 
 def write_inputs_json(all_inputs, json_output_file):
+    """Function to write the inputs JSON to a file."""
     with open(json_output_file, 'w', encoding="utf-8") as json_file:
         json.dump(all_inputs, json_file, indent=4)
 
 def move_files(file, output_dir):
+    """Function to move files to the output directory."""
     shutil.move(file, os.path.join(output_dir, file))
